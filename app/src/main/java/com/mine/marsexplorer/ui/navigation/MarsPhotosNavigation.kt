5855e35f9dada031ebe9f47732c58be55b3d0f9b
Esther Carrelle <esthcarelle@gmail.com>
2024-04-18 16:49:12 +0200

    Added a navigation class that handles network the navigation

--- app/src/main/java/com/mine/marsexplorer/ui/navigation/MarsPhotosNavigation.kt
+++ app/src/main/java/com/mine/marsexplorer/ui/navigation/MarsPhotosNavigation.kt
@@ -1,8 +1,72 @@
 package com.mine.marsexplorer.ui.navigation
 
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun MarsPhotosNavigation(toggleTheme: () -> Unit) {
-
+    val navController = rememberAnimatedNavController()
+    NavHost(navController, startDestination = Screen.Home.route) {
+        composable(
+            Screen.Home.route,
+            exitTransition = {
+                slideOutHorizontally(
+                    targetOffsetX = { MINUS_THREE_HUNDRED },
+                    animationSpec = tween(
+                        durationMillis = THREE_HUNDRED,
+                        easing = FastOutSlowInEasing
+                    )
+                ) + fadeOut(animationSpec = tween(THREE_HUNDRED))
+            },
+            popEnterTransition = {
+                slideInHorizontally(
+                    initialOffsetX = { MINUS_THREE_HUNDRED },
+                    animationSpec = tween(
+                        durationMillis = THREE_HUNDRED,
+                        easing = FastOutSlowInEasing
+                    )
+                ) + fadeIn(animationSpec = tween(THREE_HUNDRED))
+            },
+        ) {
+            MarsPhotosList(navController, hiltViewModel(), toggleTheme)
+        }
+        composable(
+            "${Screen.Details.route}${DETAILS_ROUTE}",
+            enterTransition = {
+                slideInHorizontally(
+                    initialOffsetX = { THREE_HUNDRED },
+                    animationSpec = tween(
+                        durationMillis = THREE_HUNDRED,
+                        easing = FastOutSlowInEasing
+                    )
+                ) + fadeIn(animationSpec = tween(THREE_HUNDRED))
+            },
+            exitTransition = {
+                slideOutHorizontally(
+                    targetOffsetX = { THREE_HUNDRED },
+                    animationSpec = tween(
+                        durationMillis = THREE_HUNDRED,
+                        easing = FastOutSlowInEasing
+                    )
+                ) + fadeOut(animationSpec = tween(THREE_HUNDRED))
+            },
+            arguments = listOf(navArgument(IMAGE) { type = NavType.StringType },
+                navArgument(NAME) { type = NavType.StringType },
+                navArgument(LAUNCH_DATE) { type = NavType.StringType },
+                navArgument(LANDING_DATE) { type = NavType.StringType })
+        ) {
+            MarsPhotoDetails(
+                navController,
+                hiltViewModel(),
+                it.arguments?.getString(IMAGE) ?: "",
+                it.arguments?.getString(NAME) ?: "",
+                it.arguments?.getString(LAUNCH_DATE) ?: "",
+                it.arguments?.getString(LANDING_DATE) ?: ""
+            )
+        }
+    }
 }
