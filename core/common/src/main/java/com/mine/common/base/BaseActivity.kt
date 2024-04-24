916270712f8ad3542e71211c6912adf9bab51ce7
Esther Carrelle <esthcarelle@gmail.com>
2024-04-24 14:56:34 +0200

    Created a BaseActivity and BaseViewModel

---
+++ core/common/src/main/java/com/mine/common/base/BaseActivity.kt
@@ -0,0 +1,5 @@
+package com.mine.common.base
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity()
---
+++ core/common/src/main/java/com/mine/common/base/BaseViewModel.kt
@@ -0,0 +1,5 @@
+package com.mine.common.base
+
+import androidx.lifecycle.ViewModel
+
+abstract class BaseViewModel : ViewModel()
