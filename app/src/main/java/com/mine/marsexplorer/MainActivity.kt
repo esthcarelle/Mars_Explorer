af06c3750dfc37a23a04b39b27bc0d2075d3b2df
Esther Carrelle <esthcarelle@gmail.com>
2024-04-25 12:58:18 +0200

    Instantiate an application class

--- app/src/main/java/com/mine/marsexplorer/MainActivity.kt
+++ app/src/main/java/com/mine/marsexplorer/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.mine.marsexplorer
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,22 +11,36 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.mine.common.base.BaseActivity
+import com.mine.marsexplorer.ui.navigation.MarsPhotosNavigation
 import com.mine.marsexplorer.ui.theme.MarsExplorerTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val currentTheme = isSystemInDarkTheme()
+            val toggleTheme: () -> Unit = {
+                if (currentTheme) setDayTheme() else setDarkTheme()
+            }
             MarsExplorerTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MarsPhotosNavigation(toggleTheme)
                 }
             }
         }
+    }
+
+    private fun setDayTheme() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+
+    private fun setDarkTheme() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
     }
 }
 
