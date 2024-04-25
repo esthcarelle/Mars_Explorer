89470834658c0ece0e1d7e8955ecba50c19db704
Esther Carrelle <esthcarelle@gmail.com>
2024-04-25 13:11:28 +0200

    Added strings and a screen object

--- core/design/src/main/java/com/mine/design/navigation/Screen.kt
+++ core/design/src/main/java/com/mine/design/navigation/Screen.kt
@@ -2,6 +2,8 @@ package com.mine.design.navigation
 
 import androidx.annotation.StringRes
 import com.mine.design.R
+import com.mine.utils.Constants.DETAILS
+import com.mine.utils.Constants.HOME
 
 sealed class Screen(val route: String, @StringRes val resourceId: Int) {
     object Home : Screen(HOME, R.string.text_home)
---
+++ feature/detailscreen/src/main/java/com/mine/detailscreen/RoverNameCard.kt
@@ -0,0 +1,33 @@
+package com.mine.detailscreen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun RoverNameCard(name: String) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+
+        Column(modifier = Modifier.align(Alignment.CenterVertically)) {
+            Text(
+                text = name,
+                modifier = Modifier.padding(0.dp, 0.dp, 12.dp, 0.dp),
+                color = MaterialTheme.colors.surface,
+                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.h5
+            )
+        }
+    }
+}
